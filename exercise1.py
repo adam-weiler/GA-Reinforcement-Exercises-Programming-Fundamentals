30d01b68a5648e331fcc5579040631854f592573
adam-weiler <adam.weiler@yahoo.ca>
2019-07-11 13:19:13 -0400

    Simplifying the If/Else comparisons.

--- exercise1.py
+++ exercise1.py
@@ -7,31 +7,31 @@ def display_grade(num_grade, position): #Returns percent grade and letter grade.
 
 
 def get_letter_grade(num_grade): #Based on user input, calls display_grade with appropriate reference for the list.
-    if (num_grade >= 97) and (num_grade <= 100):
+    if 97 <= num_grade < 100:
         return display_grade(num_grade, 0)
-    elif (num_grade >= 93) and (num_grade < 97):
+    elif 93 <= num_grade < 97:
         return display_grade(num_grade, 1)
-    elif (num_grade >= 90) and (num_grade < 93):
+    elif 90 <= num_grade < 93:
         return display_grade(num_grade, 2)
-    elif (num_grade >= 87) and (num_grade < 90):
+    elif 87 <= num_grade < 90:
         return display_grade(num_grade, 3)
-    elif (num_grade >= 83) and (num_grade < 87):
+    elif 83 <= num_grade < 87:
         return display_grade(num_grade, 4)
-    elif (num_grade >= 80) and (num_grade < 83):
+    elif 80 <= num_grade < 83:
         return display_grade(num_grade, 5)
-    elif (num_grade >= 77) and (num_grade < 80):
+    elif 77 <= num_grade < 80:
         return display_grade(num_grade, 6)
-    elif (num_grade >= 73) and (num_grade < 77):
+    elif 73 <= num_grade < 77:
         return display_grade(num_grade, 7)
-    elif (num_grade >= 70) and (num_grade < 73):
+    elif 70 <= num_grade < 73:
         return display_grade(num_grade, 8)
-    elif (num_grade >= 67) and (num_grade < 70):
+    elif 67 <= num_grade < 70:
         return display_grade(num_grade, 9)
-    elif (num_grade >= 63) and (num_grade < 67):
+    elif 63 <= num_grade < 67:
         return display_grade(num_grade, 10)
-    elif (num_grade >= 60) and (num_grade < 63):
+    elif 60 <= num_grade < 63:
         return display_grade(num_grade, 11)
-    elif (num_grade >= 0) and (num_grade < 60):
+    elif 0 <= num_grade < 60:
         return display_grade(num_grade, 12)
     else:
         return ('That is an invalid selection.')
